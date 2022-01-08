ba408b8b6ed836ef87a85483874611f273d573c6
Oladokun Lukman Tunde <lukmanoladokun86@gmail.com>
2022-01-08 23:21:42 +0100

    First commit: added links, set functionality to hamburgar icons

---
+++ src/components/Navbar.js
@@ -0,0 +1,40 @@
+import React, {useState}  from "react";
+import { Router } from "react-router-dom";
+import logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
+import ReorderIcon from '@material-ui/icons/Reorder';
+
+function Navbar() {
+
+  const [showLinks, setShowLinks] = useState(false);
+  function toggleNavbar(){
+    setShowLinks(!showLinks);
+  }
+
+  return (
+    <div className="navbar">
+      <div className="leftSide" id={ showLinks ? "open" : "close" }>
+        <img src={logo} alt="site logo" />
+        <div className="hiddenLinks">
+          <Link to="/">Home</Link>
+          <Link to="/menu">Menu</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+      </div>
+      <div className="rightSide">
+        <Link to="/">Home</Link>
+        <Link to="/menu">Menu</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+        <button onClick={toggleNavbar}>
+            <ReorderIcon />
+        </button>
+      </div>
+
+    </div>
+  );
+}
+
+export default Navbar;
